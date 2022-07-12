ebac1392a5f84b3aa3390db28c2be870fc31ae24
Yokoso <tomsolga90@gmail.com>
2022-07-12 14:20:49 +0200

    initialize google api library

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import clientId from '../clientId';
+
+class GoogleAuth extends React.Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: clientId,
+                scope: 'email'
+            });
+        });
+    }
+
+    render() {
+        return <div className='item'>GoogleAuth</div>
+    }
+}
+
+export default GoogleAuth;
