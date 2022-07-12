ac99c25f12f78c8ac625d62e2596f9e66e9baf24
Yokoso <tomsolga90@gmail.com>
2022-07-12 15:23:15 +0200

    auth state update

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -3,17 +3,42 @@ import React from 'react';
 import clientId from '../clientId';
 
 class GoogleAuth extends React.Component {
+    state = {
+        isSignedIn: null
+    };
+
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
                 clientId: clientId,
-                scope: 'email'
+                scope: 'email',
+                plugin_name: 'Streamz'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.onAuthChange();
+                this.auth.isSignedIn.listen(this.onAuthChange);
             });
         });
+    };
+
+    onAuthChange = () => {
+        this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+    };
+
+    renderAuthButton() {
+        if (this.state.isSignedIn === null) {
+            return <div>Checking login status</div>
+        }
+        else if (this.state.isSignedIn) {
+            return <div>Signed in</div>
+        }
+        else {
+            return <div>Signed out</div>
+        }
     }
 
     render() {
-        return <div className='item'>GoogleAuth</div>
+        return <div className='item'>{ this.renderAuthButton() }</div>
     }
 }
 
