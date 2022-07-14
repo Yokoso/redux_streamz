90ab6415d4d365203068da9a0eb20ab7fd57ac9c
Yokoso <tomsolga90@gmail.com>
2022-07-14 13:08:50 +0200

    sign in/out event handlers add

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -25,13 +25,21 @@ class GoogleAuth extends React.Component {
         this.setState({ isSignedIn: this.auth.isSignedIn.get() });
     };
 
+    onSignIn = () => {
+        this.auth.signIn();
+    };
+
+    onSignOut = () => {
+        this.auth.signOut();
+    };
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
             return null;
         }
         else if (this.state.isSignedIn) {
             return (
-                <button className='ui red google button'>
+                <button className='ui red google button' onClick={this.onSignOut}>
                     <i className='google icon' />
                     Sign out
                 </button>
@@ -39,7 +47,7 @@ class GoogleAuth extends React.Component {
         }
         else {
             return (
-                <button className='ui red google button'>
+                <button className='ui red google button' onClick={this.onSignIn}>
                     <i className='google icon' />
                     Sign in with Google
                 </button>
